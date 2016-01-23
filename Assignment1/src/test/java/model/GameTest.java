b0087b66203cddd79941d7d3ea67745b5073c8b5
Taylor Kirkpatrick <tkirkpatrick@14four.com>
2016-01-22 23:32:36 -0800

    Added Unit Tests to fulfill User Story tests.

--- Assignment1/src/test/java/model/GameTest.java
+++ Assignment1/src/test/java/model/GameTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertArrayEquals;
 
 /**
  * Created by Cameron on 1/22/2016.
@@ -28,44 +29,84 @@ public class GameTest {
 // Begin Compare portion tests ------------------------
     @Test
     public void testGameExists(){
-       Game game = new Game();
+        Deck deck = new Deck();
+        Col col1 = new Col();
+        Col col2 = new Col();
+        Col col3 = new Col();
+        Col col4 = new Col();
+        Game game = new Game(deck, col1, col2, col3, col4);
        assertNotNull(game);
     }
 
     @Test
     public void testEqualSuit(){
-        Game game = new Game();
+        Deck deck = new Deck();
+        Col col1 = new Col();
+        Col col2 = new Col();
+        Col col3 = new Col();
+        Col col4 = new Col();
+        Game game = new Game(deck, col1, col2, col3, col4);
         assertEquals(true, game.same_int(0,0));
     }
     @Test
     public void testInequalSuit(){
-        Game game = new Game();
+        Deck deck = new Deck();
+        Col col1 = new Col();
+        Col col2 = new Col();
+        Col col3 = new Col();
+        Col col4 = new Col();
+        Game game = new Game(deck, col1, col2, col3, col4);
         assertEquals(false, game.same_int(0,1));
     }
 //    // This test is meant to fail all the time, to make sure I'm actually making tests right. It should never pass.
 //    @Test
 //    public void testFailedEqual(){
-//        Game game = new Game();
+//      Deck deck = new Deck();
+//      Col col1 = new Col();
+//      Col col2 = new Col();
+//      Col col3 = new Col();
+//      Col col4 = new Col();
+//      Game game = new Game(deck, col1, col2, col3, col4);
 //        assertEquals(true, game.same_int(0,1));
 //    } // I'll comment it out when I'm finished with the feature.
     @Test
     public void testFirstNumBiggerTrue(){
-        Game game = new Game();
+        Deck deck = new Deck();
+        Col col1 = new Col();
+        Col col2 = new Col();
+        Col col3 = new Col();
+        Col col4 = new Col();
+        Game game = new Game(deck, col1, col2, col3, col4);
         assertEquals(true, game.compare_face(13,8,"g"));
     }
     @Test
     public void testFirstNumBiggerFalse(){
-        Game game = new Game();
+        Deck deck = new Deck();
+        Col col1 = new Col();
+        Col col2 = new Col();
+        Col col3 = new Col();
+        Col col4 = new Col();
+        Game game = new Game(deck, col1, col2, col3, col4);
         assertEquals(false, game.compare_face(2,8,"g"));
     }
     @Test
     public void testFirstNumSmallerTrue(){
-        Game game = new Game();
+        Deck deck = new Deck();
+        Col col1 = new Col();
+        Col col2 = new Col();
+        Col col3 = new Col();
+        Col col4 = new Col();
+        Game game = new Game(deck, col1, col2, col3, col4);
         assertEquals(true, game.compare_face(4,5,"l"));
     }
     @Test
     public void testFirstNumSmallerFalse(){
-        Game game = new Game();
+        Deck deck = new Deck();
+        Col col1 = new Col();
+        Col col2 = new Col();
+        Col col3 = new Col();
+        Col col4 = new Col();
+        Game game = new Game(deck, col1, col2, col3, col4);
         assertEquals(false, game.compare_face(5,4,"l"));
     }
     @Test   // These next two tests actually are unrelated to the compare part of this project. Just me testing making sure things work before I start using them.
@@ -80,17 +121,39 @@ public class GameTest {
     }
     @Test   // The big finisher.
     public void testCanRemoveTrue(){
-        Card fivespades = new Card(1,5);
-        Card tenspades = new Card(1,10);
-        Game game = new Game();
-        assertEquals(true, game.can_remove(fivespades,tenspades));
+        Card threeclubs = new Card(3,3);
+        Card fiveclubs = new Card(3,10);
+        Deck deck = new Deck();
+        Col col1 = new Col();
+        Col col2 = new Col();
+        Col col3 = new Col();
+        Col col4 = new Col();
+        Game game = new Game(deck, col1, col2, col3, col4);
+        assertEquals(true, game.can_remove(threeclubs,fiveclubs));
     }
     @Test
-    public void testCanRemoveFalse(){
-        Card fivespades = new Card(1,5);
-        Card tenspades = new Card(1,10);
-        Game game = new Game();
-        assertEquals(false, game.can_remove(tenspades,fivespades));
+    public void testCanRemoveFalseFace(){
+        Card threeclubs = new Card(3,3);
+        Card twoclubs = new Card(3,2);
+        Deck deck = new Deck();
+        Col col1 = new Col();
+        Col col2 = new Col();
+        Col col3 = new Col();
+        Col col4 = new Col();
+        Game game = new Game(deck, col1, col2, col3, col4);
+        assertEquals(false, game.can_remove(threeclubs,twoclubs));
+    }
+    @Test
+    public void testCanRemoveFalseSuit(){
+        Card threeclubs = new Card(3,3);
+        Card fourspades = new Card(1,2);
+        Deck deck = new Deck();
+        Col col1 = new Col();
+        Col col2 = new Col();
+        Col col3 = new Col();
+        Col col4 = new Col();
+        Game game = new Game(deck, col1, col2, col3, col4);
+        assertEquals(false, game.can_remove(threeclubs,fourspades));
     }
 // End Compare portion tests ---------------------------------------------
     }
